58cac3a4b4e0b3c7d95a37c24fa2fc48ec7717f3
Sean M. Lohman <smlohman@fullsail.edu>
2018-01-12 22:33:15 -0500

    Updated MapView
    Added a more defined starting location on the map view. Currently auto
    set to full sail university.
    
    Played around with the region of the map, meaning how big of a area the
    map will show when a zip code or the users location is set. Tried 500,
    but that was too zoomed in, so I tried 1500, and it worked well.

--- MarketMate/MarketMate/ViewController.swift
+++ MarketMate/MarketMate/ViewController.swift
@@ -17,15 +17,24 @@ class ViewController: UIViewController {
     
     /* IBActions */
     
-    /* Default Methods */
+    /* Methods */
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        //Creating an Initial Location For testing purposes. Initial Location: Full Sail University
+        let initialLocation = CLLocation(latitude: 28.5962, longitude: -81.3064)
+        
+        //Centering Map on the initial location. TO:DO - get the users location and replace hardcoded location in ViewDidLoad
+        func centerMapOnLocation(location: CLLocation) {
+            //Setting the Default Zoom radius to 1500 so the user isnt presented with a map of the united states on launch
+            let regionRadius: CLLocationDistance = 1500
+            let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate, regionRadius, regionRadius)
+            MapKitView.setRegion(coordinateRegion, animated: true)
+        }
+        
+        //Calling Method Created Below
+        centerMapOnLocation(location: initialLocation)
     }
 
 
